48ea215e99bf2cf01fe7b616faaca04de4a57169
Nader Aeinehchi <Nader.Aeinehchi@skatteetaten.no>
2023-11-27 16:33:26 +0100

    adds rule engine and maven. But Maven does not work

--- src/main/java/RuleEngine.java
+++ src/main/java/RuleEngine.java
@@ -1,6 +1,5 @@
 package main.java;
 
-// import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -8,25 +7,26 @@ import java.util.PriorityQueue;
 public class RuleEngine {
 
     private final List<Rule> rules;
+    private final PriorityQueue<Rule> matchingRules;
 
     public RuleEngine(List<Rule> rules) {
         this.rules = rules;
+        matchingRules = new PriorityQueue<>((rule1, rule2) -> Integer.compare(rule2.getId(), rule1.getId()));
+
     }
 
     public Rule evaluate(String condition, String... parameters) {
-        PriorityQueue<Rule> matchingRules = new PriorityQueue<>((rule1, rule2) -> Integer.compare(rule2.getId(), rule1.getId()));
 
         for (Rule rule : rules) {
             if (rule.getCondition().equals(condition) && Arrays.equals(rule.getParameters(), parameters)) {
-                matchingRules.add(rule);
+                this.matchingRules.add(rule);
             }
         }
 
         if (!matchingRules.isEmpty()) {
-            return matchingRules.poll();
+            return this.matchingRules.poll();
         }
 
         return null;
     }
 }
-
