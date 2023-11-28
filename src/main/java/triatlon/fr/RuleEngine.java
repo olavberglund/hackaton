0edd82f418d13976941a0a5dde2cd31186fbf646
Nader Aeinehchi <Nader.Aeinehchi@skatteetaten.no>
2023-11-28 16:38:59 +0100

    Modifies Rule engine

--- src/main/java/triatlon/fr/RuleEngine.java
+++ src/main/java/triatlon/fr/RuleEngine.java
@@ -1,33 +1,27 @@
 package triatlon.fr;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class RuleEngine {
 
-    private final List<Rule> rules;
-    private final PriorityQueue<Rule> matchingRules;
+    private final PriorityQueue<Rule> rules;
 
     public RuleEngine(List<Rule> rules) {
-        this.rules = rules;
-        matchingRules = new PriorityQueue<>((rule1, rule2) -> Integer.compare(rule2.getId(), rule1.getId()));
-
+        this.rules = new PriorityQueue<>((rule1, rule2) -> Integer.compare(rule2.getId(), rule1.getId()));
     }
 
-    public Rule evaluate(String condition, String... parameters) {
+    public Action evaluate(String conditionDescription, String... parameters) {
+        //FIXME: Implement a logic to handel parameters
 
         for (Rule rule : rules) {
-            if (rule.getCondition().equals(condition) && Arrays.equals(rule.getParameters(), parameters)) {
-                return rule.getAction()
-                // this.matchingRules.add(rule);
+            Action action = rule.getAction(conditionDescription);
+
+            if (action != Action.NO_ACTION) {
+                return action;
             }
         }
+        return Action.NO_ACTION;
 
-        if (!matchingRules.isEmpty()) {
-            return this.matchingRules.poll();
-        }
-
-        return null;
     }
 }
