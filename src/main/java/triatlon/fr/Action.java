f0cd21c4376101deb32f4b00d82a757b46a4c2ab
Nader Aeinehchi <Nader.Aeinehchi@skatteetaten.no>
2023-11-28 16:06:13 +0100

    Adds Action enum

--- src/main/java/triatlon/fr/Action.java
+++ src/main/java/triatlon/fr/Action.java
@@ -6,7 +6,8 @@ public enum Action {
      */
     CONTINUE("Fortsett registrering"),
     CANCEL("Avbryt registrering"),
-    MANUAL("Manuell vurdering");
+    MANUAL("Manuell vurdering"),
+    NO_ACTION("INGEN HANDLING");
 
     private final String action;
 
