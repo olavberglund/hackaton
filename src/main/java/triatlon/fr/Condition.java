311149b49df8965c3a9c7f6d064e294e1889624b
Nader Aeinehchi <Nader.Aeinehchi@skatteetaten.no>
2023-11-28 15:51:18 +0100

    Adds condition enum

---
+++ src/main/java/triatlon/fr/Condition.java
@@ -0,0 +1,11 @@
+package triatlon.fr;
+
+public enum Condition {
+    IDENTIFICATOR_GIVEN("Identifikator oppgitt");
+
+    private final String condition;
+
+    private Condition(String condition) {
+        this.condition = condition;
+    }
+}
