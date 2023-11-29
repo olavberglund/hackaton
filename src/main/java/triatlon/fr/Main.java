2def986f01b8b9d245fddfc172a7d7d171f72e20
Tasmia Batool Hussain <TasmiaBatool.Hussain@skatteetaten.no>
2023-11-29 10:52:33 +0100

    justeringer

--- src/main/java/triatlon/fr/Main.java
+++ src/main/java/triatlon/fr/Main.java
@@ -21,30 +21,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rule rule2 = new Rule(2, IDENTIFIKATOR_ER_OPPGITT_OG_GJENKJENNBAR, Action.CONTINUE);
-        Rule rule6 = new Rule(6, TILFLYTNINGSLAND_IKKE_OPPGITT, Action.CANCEL);
-        Rule rule7 = new Rule(7, TILFLYTNINGSLAND_ER_OPPGITT_OG_GYLDIG, Action.CONTINUE);
-        Rule rule10 = new Rule(10, DET_OPPGIVES_OM_PERSON_HAR_EGEN_SELVSTENDIG_BOLIG, Action.CONTINUE);
-        Rule rule15 = new Rule(15, PERSONEN_HAR_EKTEFELLE_I_EN_NORSK_KOMMUNE, Action.MANUAL);
-        Rule rule17 = new Rule(17, PERSONEN_HAR_INGEN_EKTEFELLE_ELLER_BARN_I_EN_NORSK_KOMMUNE, Action.CONTINUE);
-        Rule rule18 = new Rule(18, PERSONEN_HAR_ARBEIDSMESSIG_TILKNYTNING_I_EN_NORSK_KOMMUNE, Action.CANCEL);
-        Rule rule19 = new Rule(19, PERSONEN_HAR_INGEN_ARBEIDSMESSIG_TILKNYTNING_I_EN_NORSK_KOMMUNE, Action.CONTINUE);
-        Rule rule20 = new Rule(20, PERSONEN_PLANLEGGER_A_OPPHOLDE_SEG_DELER_AV_AARET_I_NORGE, Action.MANUAL);
-        Rule rule23 = new Rule(23, PERSONEN_OPPGIR_MAKS_ANTALL_DAGER_I_STREKK, Action.CONTINUE);
-        Rule rule25 = new Rule(25, PERSONEN_OPPGIR_AT_HEN_IKKE_SKAL_OPPHOLDE_SEG_I_NORGE, Action.CONTINUE);
-
         List<Rule> rules = new ArrayList<>() {{
-            add(rule2);
-            add(rule6);
-            add(rule7);
-            add(rule10);
-            add(rule15);
-            add(rule17);
-            add(rule18);
-            add(rule19);
-            add(rule20);
-            add(rule23);
-            add(rule25);
+            add(new Rule(2, IDENTIFIKATOR_ER_OPPGITT_OG_GJENKJENNBAR, Action.CONTINUE));
+            add(new Rule(6, TILFLYTNINGSLAND_IKKE_OPPGITT, Action.CANCEL));
+            add(new Rule(7, TILFLYTNINGSLAND_ER_OPPGITT_OG_GYLDIG, Action.CONTINUE));
+            add(new Rule(10, DET_OPPGIVES_OM_PERSON_HAR_EGEN_SELVSTENDIG_BOLIG, Action.CONTINUE));
+            add(new Rule(15, PERSONEN_HAR_EKTEFELLE_I_EN_NORSK_KOMMUNE, Action.MANUAL));
+            add(new Rule(17, PERSONEN_HAR_INGEN_EKTEFELLE_ELLER_BARN_I_EN_NORSK_KOMMUNE, Action.CONTINUE));
+            add(new Rule(18, PERSONEN_HAR_ARBEIDSMESSIG_TILKNYTNING_I_EN_NORSK_KOMMUNE, Action.CANCEL));
+            add(new Rule(19, PERSONEN_HAR_INGEN_ARBEIDSMESSIG_TILKNYTNING_I_EN_NORSK_KOMMUNE, Action.CONTINUE));
+            add(new Rule(20, PERSONEN_PLANLEGGER_A_OPPHOLDE_SEG_DELER_AV_AARET_I_NORGE, Action.MANUAL));
+            add(new Rule(23, PERSONEN_OPPGIR_MAKS_ANTALL_DAGER_I_STREKK, Action.CONTINUE));
+            add(new Rule(25, PERSONEN_OPPGIR_AT_HEN_IKKE_SKAL_OPPHOLDE_SEG_I_NORGE, Action.CONTINUE));
         }};
 
         RuleEngine ruleEngine = new RuleEngine(rules);
